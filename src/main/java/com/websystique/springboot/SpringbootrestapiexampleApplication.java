2332e13864292ac8fbed8c1c4445af1abf6d59b1
tom-tong <tongmu1@hotmail.com>
2017-03-24 00:41:11 +1300

    v1.1

---
+++ src/main/java/com/websystique/springboot/SpringbootrestapiexampleApplication.java
@@ -0,0 +1,12 @@
+package com.websystique.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestapiexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestapiexampleApplication.class, args);
+	}
+}
