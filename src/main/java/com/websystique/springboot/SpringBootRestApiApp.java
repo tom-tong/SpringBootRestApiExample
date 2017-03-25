7b7ca3f717bbd67ba3c2e48fa44dc2512917f2c6
tom-tong <tongmu1@hotmail.com>
2017-03-26 00:35:26 +1300

    v。8

--- src/main/java/com/websystique/springboot/SpringBootRestApiApp.java
+++ src/main/java/com/websystique/springboot/SpringBootRestApiApp.java
@@ -2,12 +2,19 @@ package com.websystique.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication(scanBasePackages={"com.websystique.springboot"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
-public class SpringBootRestApiApp {
+public class SpringBootRestApiApp extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootRestApiApp.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApp.class, args);
 	}
 }
+
